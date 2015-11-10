16f5b0dec8fd732720c0e157ed4882b1432ba537
somepad <somepad@users.noreply.github.com>
2015-11-10 13:06:47 +0300

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cpan App::cpanminus
+sudo hhru-cvupdater-perl/install/setup.sh
 cpanm -M https://cpan.metacpan.org -n Mojolicious
-cpanm --installdeps .
+cpanm -M https://cpan.metacpan.org --installdeps .
 cp ../c_v_updater.conf_template ../c_v_updater.conf
